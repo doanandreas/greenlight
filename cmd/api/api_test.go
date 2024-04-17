a8ef09de5077e4522e62fb4dae3f7b2ce99da736
Doan Andreas Nathanael (doan.a) <doan.a@samsung.com>
2024-04-17 08:38:31 +0700

    add test for create movie

--- cmd/api/api_test.go
+++ cmd/api/api_test.go
@@ -26,3 +26,22 @@ func Test_HealthCheck(t *testing.T) {
 		t.Errorf("got %s; expected 'status: available'", string(sut))
 	}
 }
+
+func Test_CreateMovie(t *testing.T) {
+	var app application
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/v1/movies", nil)
+
+	handler := http.HandlerFunc(app.createMovieHandler)
+	handler.ServeHTTP(w, r)
+
+	sut, err := io.ReadAll(w.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !strings.Contains(string(sut), "create a new movie") {
+		t.Errorf("got %s; expected 'create a new movie'", string(sut))
+	}
+}
