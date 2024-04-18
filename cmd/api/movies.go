2e20476235acad8824ff7299492e3715edc960cc
Doan Andreas Nathanael (doan.a) <doan.a@samsung.com>
2024-04-18 10:14:26 +0700

    use json for show movie handler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"greenlight.doanandreas.net/internal/data"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -12,11 +14,26 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	id, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 64)
 	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := map[string]data.Movie{
+		"movie": {
+			ID:        id,
+			CreatedAt: time.Now(),
+			Title:     "Casablanca",
+			Runtime:   102,
+			Genres:    []string{"drama", "romance", "war"},
+			Version:   1,
+		},
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
