302fce3e5124744f1e8eb52ac77c9a04f3fc83a3
Doan Andreas Nathanael (doan.a) <doan.a@samsung.com>
2024-04-19 10:45:29 +0700

    add unmarshal json for runtime

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -3,6 +3,7 @@ package data
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type Runtime int32
@@ -11,4 +12,16 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(data []byte) error {
+	trimmedStr := strings.Trim(string(data), `"`)
+	strValue := strings.Split(trimmedStr, " ")[0]
+	intValue, err := strconv.ParseInt(strValue, 10, 32)
+	if err != nil {
+		return err
+	}
+
+	*r = Runtime(int32(intValue))
+	return nil
 }
